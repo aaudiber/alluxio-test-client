4b74b1ef92721fd96f575a58abfde4292e9233c6
Andrew Audibert <andrew@alluxio.com>
2018-10-24 16:01:47 -0700

    Add test client and README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Build
+
+```
+mvn package
+```
+
+# Run
+
+```
+# Generate an 80M file
+dd if=/dev/zero of=80M count=$[80*1024] bs=1024
+# Copy data to /test directory within Alluxio
+for i in `seq 1 10`; do ${ALLUXIO_HOME}/bin/alluxio fs copyFromLocal 80M /test/$i; done
+
+# Read data
+java -cp 'target/alluxio-test-client-1.0-SNAPSHOT.jar:/path/to/alluxio-client.jar' alluxio.TestClient /test
+```
---
+++ src/main/java/alluxio/TestClient.java
@@ -0,0 +1,26 @@
+package alluxio;
+
+import alluxio.client.file.FileInStream;
+import alluxio.client.file.FileSystem;
+import alluxio.client.file.URIStatus;
+
+/**
+ * Repeatedly read the contents of the specified directory.
+ */
+public class TestClient {
+  public static void main(String[] args) throws Throwable {
+    FileSystem fs = FileSystem.Factory.get();
+    String dir = args[0];
+    while (true) {
+      for (URIStatus path : fs.listStatus(new AlluxioURI(dir))) {
+        if (path.isFolder()) {
+          continue;
+        }
+        FileInStream in = fs.openFile(new AlluxioURI(path.getPath()));
+        byte[] buf = new byte[4096];
+        while (in.read(buf) >= 0);
+        System.out.printf("Finished reading %s\n", path.getPath());
+      }
+    }
+  }
+}
